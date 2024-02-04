16a92360e754bcb516906d9271d55247a2863850
Matt Linder <linder2015@outlook.com>
2024-02-04 15:08:16 -0500

    test

--- routes/file.ts
+++ routes/file.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { client } from "../config";
 
 const router = Router();
 
-router.get("/", (req, res) => res.status(200).json("ok"));
+router.get("/", async (req, res) => {
+  await client.execute("select * from users");
+  res.status(200).json("ok");
+});
 
 export default router;
